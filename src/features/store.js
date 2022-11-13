b68cdfaeb99c498e5f8ae5e652e01bbbf31ef2b7
Francisco Garciarena <80267392+Francisco-07@users.noreply.github.com>
2022-11-13 15:05:59 -0300

    homepage, transactions redux, skeletons (#6)

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth/authSlice'
+import accountReducer from './account/accountSlice'
+import transactionReducer from './transaction/transactionSlice'
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    account: accountReducer,
+    transaction: transactionReducer,
   },
 })

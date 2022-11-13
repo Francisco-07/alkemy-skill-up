48086cabf0bbca210aeca919f99b91b54e5c0191
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-11-13 14:53:19 -0300

    homepage, transactions redux, skeletons

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,7 +1,14 @@
+// Components
+import Dashboard from '../../components/dashboard/Dashboard'
+
 // Styles
 import styled from './homePage.module.css'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <section className={styled.container}>
+      <Dashboard />
+    </section>
+  )
 }
 export default HomePage

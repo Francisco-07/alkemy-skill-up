a551b49e8054f5c0097891dac2c011de060cd61d
Francisco Garciarena <80267392+Francisco-07@users.noreply.github.com>
2022-11-13 14:24:36 -0300

    base structure, added routes, title and btn (#1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+// Pages
+import HomePage from './pages/home/HomePage'
+
+// Libraries
+import { Navigate, Outlet, Routes, Route } from 'react-router-dom'
+
+const PrivateRoutes = () => {
+  const token = JSON.parse(localStorage.getItem('token'))
+  return token?.accessToken ? <Outlet /> : <Navigate to='/login' />
+}
+
 function App() {
-  return <div>asd</div>
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <main>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route exact path='/' element={<HomePage />} />
+            {/* <Route path='/users' element={<UsersListPage />} />
+            <Route path='/user' element={<UserPage />} />
+            <Route path='/charge' element={<ChargeMoneyPage />} />
+            <Route path='/transactions' element={<TransactionsPage />} />
+            <Route path='/transaction' element={<Transaction />} /> */}
+          </Route>
+          {/* <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='*' element={<ErrorPage />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,7 @@
+// Styles
+import styled from './homePage.module.css'
+
+const HomePage = () => {
+  return <div>HomePage</div>
+}
+export default HomePage
